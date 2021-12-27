179c9d9547b74f018a4ca5c3f970244146990c32
catlair <catlair@qq.com>
2021-12-27 17:41:33 +0800

    chore: jsconfig env proxy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,34 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.resolve(__dirname, dir)
+}
+
 module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        additionalData: `@import "@/assets/styles/_variables.scss";`
+        additionalData: `@import "@/assets/styles/variables.scss";`
       }
     }
   },
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.resolve.alias.set('#', resolve('src/types'))
+    config.resolve.alias.set('node_modules', resolve('node_modules'))
+  },
+  devServer: {
+    port: process.env.PORT || 8080,
+    // https://github.com/chimurai/http-proxy-middleware#proxycontext-config
+    proxy: {
+      '/basic-api': {
+        target: process.env.VUE_APP_PROXY_URL,
+        changeOrigin: true,
+        // 重写路径，否则访问路径 /basic-api
+        pathRewrite: {
+          '^/basic-api': ''
+        }
+      }
+    }
+  }
 }
